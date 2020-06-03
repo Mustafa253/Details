1fd9960267a447b04dc8e1bf1ecadb68ef8309c5
Mustafa253 <63819764+Mustafa253@users.noreply.github.com>
2020-06-03 05:37:35 -0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+ 
+#this is a comment
+urlpatterns = [
+   path('', views.index, name='index'),
+   path('getTypes/', views.getTypes, name='types'),
+   path('getProducts/', views.getProducts, name='products'),
+]
