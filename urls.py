07cd1b20463d3eff231df49344e736828fbf0818
Mustafa253 <63819764+Mustafa253@users.noreply.github.com>
2020-06-03 05:38:37 -0700

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
- 
-#this is a comment
-urlpatterns = [
-   path('', views.index, name='index'),
-   path('getTypes/', views.getTypes, name='types'),
-   path('getProducts/', views.getProducts, name='products'),
-]
